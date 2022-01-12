6605917883a1d7b0bcf72b18f68d0524031630de
Jeffrey Prado <Jefgapra@espol.edu.ec>
2022-01-12 18:22:48 -0500

    gcs tested

--- models/gcs.js
+++ models/gcs.js
@@ -1,88 +1,60 @@
+const { Spanner } = require('@google-cloud/spanner');
+
 const config = {
     project_id: 'fair-bearing-332523',
     instancia_id: 'aerolineadba',
     database_id: 'proyectofinaldba',
 }
 
+const getColumns = (table) => {
+    var r = {
+        'passenger' : ['passid', 'passname', 'passemail', 'passdob', 'shardid'],
+        'bookingdetails' : ['bookingid', 'passid'],
+        'booking' : ['bookingid', 'flightid', 'bookdate', 'shardid'],
+        'flight' : ['flightid', 'flightsource', 'flightdest', 'flightdete', 'flightseat', 'ticketcost']
+    }
+    return r[table];
+};
 
 /* CREATE */
-// async function insertData(instanceId, databaseId, projectId) {
-//     // [START spanner_insert_data]
-//     // Imports the Google Cloud client library
-//     const { Spanner } = require('@google-cloud/spanner');
-
-//     /**
-//      * TODO(developer): Uncomment the following lines before running the sample.
-//      */
-//     // const projectId = 'my-project-id';
-//     // const instanceId = 'my-instance';
-//     // const databaseId = 'my-database';
-
-//     // Creates a client
-//     const spanner = new Spanner({
-//         projectId: projectId,
-//     });
-
-//     // Gets a reference to a Cloud Spanner instance and database
-//     const instance = spanner.instance(instanceId);
-//     const database = instance.database(databaseId);
-
-//     // Instantiate Spanner table objects
-//     const singersTable = database.table('Singers');
-//     const albumsTable = database.table('Albums');
-
-//     // Inserts rows into the Singers table
-//     // Note: Cloud Spanner interprets Node.js numbers as FLOAT64s, so
-//     // they must be converted to strings before being inserted as INT64s
-//     try {
-//         await singersTable.insert([
-//             { SingerId: '1', FirstName: 'Marc', LastName: 'Richards' },
-//             { SingerId: '2', FirstName: 'Catalina', LastName: 'Smith' },
-//             { SingerId: '3', FirstName: 'Alice', LastName: 'Trentor' },
-//             { SingerId: '4', FirstName: 'Lea', LastName: 'Martin' },
-//             { SingerId: '5', FirstName: 'David', LastName: 'Lomond' },
-//         ]);
-
-//         await albumsTable.insert([
-//             { SingerId: '1', AlbumId: '1', AlbumTitle: 'Total Junk' },
-//             { SingerId: '1', AlbumId: '2', AlbumTitle: 'Go, Go, Go' },
-//             { SingerId: '2', AlbumId: '1', AlbumTitle: 'Green' },
-//             { SingerId: '2', AlbumId: '2', AlbumTitle: 'Forever Hold your Peace' },
-//             { SingerId: '2', AlbumId: '3', AlbumTitle: 'Terrified' },
-//         ]);
-
-//         console.log('Inserted data.');
-//     } catch (err) {
-//         console.error('ERROR:', err);
-//     } finally {
-//         await database.close();
-//     }
-//     // [END spanner_insert_data]
-// }
-
-/* READ */
-async function readData(instanceId, databaseId, projectId) {
-    const { Spanner } = require('@google-cloud/spanner');
-
+async function insertData(instanceId, databaseId, projectId, tableName, json) {
     const spanner = new Spanner({
         projectId: projectId,
     });
-
     const instance = spanner.instance(instanceId);
     const database = instance.database(databaseId);
 
-    const passTable = database.table('passenger');
+    const tableN = database.table(tableName);
 
-    const query = {
-        columns: ['passid', 'passname', 'passemail', 'passdob', 'shardid'],
+    try {
+        await tableN.insert(json);
+        console.log('Inserted data.');
+    } catch (err) {
+        console.error('ERROR:', err);
+    } finally {
+        await database.close();
+    }
+}
+
+/* READ */
+async function readData(instanceId, databaseId, projectId, tableName) {
+    const spanner = new Spanner({
+        projectId: projectId,
+    });
+    const instance = spanner.instance(instanceId);
+    const database = instance.database(databaseId);
+
+    const tableN = database.table(tableName);
+
+    const query = { 
+        columns: getColumns(tableName),
         keySet: {
             all: true,
         },
     };
 
     try {
-        const [rows] = await passTable.read(query);
-
+        const [rows] = await tableN.read(query);
         rows.forEach(row => {
             const json = row.toJSON();
             console.log(json);
@@ -94,209 +66,121 @@ async function readData(instanceId, databaseId, projectId) {
     }
 }
 
-// async function queryData(instanceId, databaseId, projectId) {
-//     // [START spanner_query_data]
-//     // Imports the Google Cloud client library
-//     const { Spanner } = require('@google-cloud/spanner');
+async function queryData(instanceId, databaseId, projectId, tableName, tableId, id) {
+    const spanner = new Spanner({
+        projectId: projectId,
+    });
+    const instance = spanner.instance(instanceId);
+    const database = instance.database(databaseId);
 
-//     /**
-//      * TODO(developer): Uncomment the following lines before running the sample.
-//      */
-//     // const projectId = 'my-project-id';
-//     // const instanceId = 'my-instance';
-//     // const databaseId = 'my-database';
+    const query = {
+        sql: 'SELECT * FROM ' + tableName + ' WHERE ' + tableId + ' = @searchId' ,
+        params: {
+            searchId: id,
+        }
+    };
 
-//     // Creates a client
-//     const spanner = new Spanner({
-//         projectId: projectId,
-//     });
-
-//     // Gets a reference to a Cloud Spanner instance and database
-//     const instance = spanner.instance(instanceId);
-//     const database = instance.database(databaseId);
-
-//     const query = {
-//         sql: 'SELECT SingerId, AlbumId, AlbumTitle FROM Albums',
-//     };
-
-//     // Queries rows from the Albums table
-//     try {
-//         const [rows] = await database.run(query);
-
-//         rows.forEach(row => {
-//             const json = row.toJSON();
-//             console.log(
-//                 `SingerId: ${json.SingerId}, AlbumId: ${json.AlbumId}, AlbumTitle: ${json.AlbumTitle}`
-//             );
-//         });
-//     } catch (err) {
-//         console.error('ERROR:', err);
-//     } finally {
-//         // Close the database when finished.
-//         await database.close();
-//     }
-//     // [END spanner_query_data]
-// }
-
-// async function readStaleData(instanceId, databaseId, projectId) {
-//     // [START spanner_read_stale_data]
-//     // Imports the Google Cloud client library
-//     const { Spanner } = require('@google-cloud/spanner');
-
-//     /**
-//      * TODO(developer): Uncomment the following lines before running the sample.
-//      */
-//     // const projectId = 'my-project-id';
-//     // const instanceId = 'my-instance';
-//     // const databaseId = 'my-database';
-
-//     // Creates a client
-//     const spanner = new Spanner({
-//         projectId: projectId,
-//     });
-
-//     // Gets a reference to a Cloud Spanner instance and database
-//     const instance = spanner.instance(instanceId);
-//     const database = instance.database(databaseId);
-
-//     // Reads rows from the Albums table
-//     const albumsTable = database.table('Albums');
-
-//     const query = {
-//         columns: ['SingerId', 'AlbumId', 'AlbumTitle', 'MarketingBudget'],
-//         keySet: {
-//             all: true,
-//         },
-//     };
-
-//     const options = {
-//         // Guarantees that all writes committed more than 15 seconds ago are visible
-//         exactStaleness: 15,
-//     };
-
-//     try {
-//         const [rows] = await albumsTable.read(query, options);
-
-//         rows.forEach(row => {
-//             const json = row.toJSON();
-//             const id = json.SingerId;
-//             const album = json.AlbumId;
-//             const title = json.AlbumTitle;
-//             const budget = json.MarketingBudget ? json.MarketingBudget : '';
-//             console.log(
-//                 `SingerId: ${id}, AlbumId: ${album}, AlbumTitle: ${title}, MarketingBudget: ${budget}`
-//             );
-//         });
-//     } catch (err) {
-//         console.error('ERROR:', err);
-//     } finally {
-//         // Close the database when finished.
-//         await database.close();
-//     }
-//     // [END spanner_read_stale_data]
-// }
-
-// /* UPDATE */
-// async function updateData(instanceId, databaseId, projectId) {
-//     const { Spanner } = require('@google-cloud/spanner');
-
-//     const spanner = new Spanner({
-//         projectId: projectId,
-//     });
-
-//     const instance = spanner.instance(instanceId);
-//     const database = instance.database(databaseId);
-
-//     const albumsTable = database.table('Albums');
-
-//     try {
-//         await albumsTable.update([
-//             { SingerId: '1', AlbumId: '1', MarketingBudget: '100000' },
-//             { SingerId: '2', AlbumId: '2', MarketingBudget: '500000' },
-//         ]);
-//         console.log('Updated data.');
-//     } catch (err) {
-//         console.error('ERROR:', err);
-//     } finally {
-//         database.close();
-//     }
-// }
-
-// /* DELETE */
-// async function deleteData(instanceId, databaseId, projectId) {
-//     // [START spanner_delete_data]
-//     // Imports the Google Cloud client library
-//     const { Spanner } = require('@google-cloud/spanner');
-
-//     /**
-//      * TODO(developer): Uncomment the following lines before running the sample.
-//      */
-//     // const projectId = 'my-project-id';
-//     // const instanceId = 'my-instance';
-//     // const databaseId = 'my-database';
-
-//     // Creates a client
-//     const spanner = new Spanner({
-//         projectId: projectId,
-//     });
-
-//     // Gets a reference to a Cloud Spanner instance and database
-//     const instance = spanner.instance(instanceId);
-//     const database = instance.database(databaseId);
-
-//     // Instantiate Spanner table object
-//     const albumsTable = database.table('Albums');
-
-//     // Deletes individual rows from the Albums table.
-//     try {
-//         const keys = [
-//             [2, 1],
-//             [2, 3],
-//         ];
-//         await albumsTable.deleteRows(keys);
-//         console.log('Deleted individual rows in Albums.');
-//     } catch (err) {
-//         console.error('ERROR:', err);
-//     }
-
-//     // Delete a range of rows where the column key is >=3 and <5
-//     database.runTransaction(async (err, transaction) => {
-//         if (err) {
-//             console.error(err);
-//             return;
-//         }
-//         try {
-//             const [rowCount] = await transaction.runUpdate({
-//                 sql: 'DELETE FROM Singers WHERE SingerId >= 3 AND SingerId < 5',
-//             });
-//             console.log(`${rowCount} records deleted from Singers.`);
-//         } catch (err) {
-//             console.error('ERROR:', err);
-//         }
-
-//         // Deletes remaining rows from the Singers table and the Albums table,
-//         // because Albums table is defined with ON DELETE CASCADE.
-//         try {
-//             // The WHERE clause is required for DELETE statements to prevent
-//             // accidentally deleting all rows in a table.
-//             // https://cloud.google.com/spanner/docs/dml-syntax#where_clause
-//             const [rowCount] = await transaction.runUpdate({
-//                 sql: 'DELETE FROM Singers WHERE true',
-//             });
-//             console.log(`${rowCount} records deleted from Singers.`);
-//             await transaction.commit();
-//         } catch (err) {
-//             console.error('ERROR:', err);
-//         } finally {
-//             // Close the database when finished.
-//             await database.close();
-//         }
-//     });
-//     // [END spanner_delete_data]
-// }
-
-readData(config.instancia_id, config.database_id, config.project_id).then(
-    res => {
-        console.log(res);
+    try {
+        const [rows] = await database.run(query);
+        rows.forEach(row => {
+            const json = row.toJSON();
+            console.log(json);
+        });
+    } catch (err) {
+        console.error('ERROR:', err);
+    } finally {
+        await database.close();
     }
-);
+}
+
+/* UPDATE */
+async function updateData(instanceId, databaseId, projectId, tableName, rows) {
+    const spanner = new Spanner({
+        projectId: projectId,
+    });
+    const instance = spanner.instance(instanceId);
+    const database = instance.database(databaseId);
+
+    const tableN = database.table(tableName);
+
+    try {
+        /* rows es array [{ SingerId: '1', AlbumId: '1', MarketingBudget: '100000' }] */
+        await tableN.update(rows);
+        console.log('Updated data.');
+    } catch (err) {
+        console.error('ERROR:', err);
+    } finally {
+        database.close();
+    }
+}
+
+/* DELETE */
+async function deleteData(instanceId, databaseId, projectId, tableName, ids) {
+    const spanner = new Spanner({
+        projectId: projectId,
+    });
+    const instance = spanner.instance(instanceId);
+    const database = instance.database(databaseId);
+
+    const tableN = database.table(tableName);
+
+    try {
+        /* ids [[shardid, tableId] - [bookingid, passid] - [id]
+                [2, 3],]; */
+        await tableN.deleteRows(ids);
+        console.log('Deleted individual rows in Albums.');
+    } catch (err) {
+        console.error('ERROR:', err);
+    }
+
+    // database.runTransaction(async (err, transaction) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return;
+    //     }
+    //     try {
+    //         const [rowCount] = await transaction.runUpdate({
+    //             sql: 'DELETE FROM Singers WHERE true',
+    //         });
+    //         console.log(`${rowCount} records deleted from Singers.`);
+    //         await transaction.commit();
+    //     } catch (err) {
+    //         console.error('ERROR:', err);
+    //     } finally {
+    //         // Close the database when finished.
+    //         await database.close();
+    //     }
+    // });
+}
+
+// bjson = require('../config/booking.json');
+// fjson = require('../config/flight.json');
+// pjson = require('../config/passenger.json');
+
+// pjson = [
+//     {shardid:1,passid:1,passname:"Nathan Whitney",passemail:"ultricies@icloud.org",passdob:"2022-10-08"}
+// ]
+
+/*
+insertData(config.instancia_id, config.database_id, config.project_id, 'passenger', pjson);
+*/
+
+/*
+readData(config.instancia_id, config.database_id, config.project_id, 'passenger');
+*/
+
+// queryData(config.instancia_id, config.database_id, config.project_id, 'passenger', 'passid', 1);
+
+// update = [{
+//     shardid: 1,
+//     passid: 1,
+//     passname: 'Jeffrey Prado',
+//     passemail: 'jeffgapra@espol.edu',
+//     passdob: "2023-12-12"
+// }]
+
+// updateData(config.instancia_id, config.database_id, config.project_id, 'passenger', update);
+
+// deleteData(config.instancia_id, config.database_id, config.project_id, 'passenger', [[1,1]]);
+
+// readData(config.instancia_id, config.database_id, config.project_id, 'passenger');
